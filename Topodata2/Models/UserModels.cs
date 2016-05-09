3d73c1c5de2452e47cfa9368fd3f2ef5d25d8ec0
kelfibm <kelfi.bm@gmail.com>
2016-05-09 09:25:58 -0400

    UserModels added with attributies

---
+++ Topodata2/Models/UserModels.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Security;
+
+namespace Topodata2.Models
+{
+    public class UserModels
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Email Address")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(30, MinimumLength = 5)]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MembershipPassword]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        [MembershipPassword]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "Would you like to receive news about Topodata?")]
+        [Range(typeof(bool),"false","true")]
+        public bool Informado { get; set; }
+
+    }
+
+}
