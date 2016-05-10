1938daf79a5fdcd2dbe0a42f6aa5ee8caf089b8a
kelfibm <kelfi.bm@gmail.com>
2016-05-10 10:43:18 -0400

    UserViewModel added UserModels name changed and error message added Register view: model added, textboxfor Register name added

--- Topodata2/Models/UserModels.cs
+++ Topodata2/Models/UserModels.cs
@@ -10,38 +10,38 @@ namespace Topodata2.Models
 
     public class RegisterUserViewModel
     {
-        [Required]
-        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [Display(Name = "Nombre")]
         [DataType(DataType.Text)]
         public string Name { get; set; }
 
-        [Required]
-        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [Display(Name = "Apellido")]
         [DataType(DataType.Text)]
         public string LastName { get; set; }
 
-        [Required]
-        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [Display(Name = "Email")]
         [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Este campo es requerido")]
         [DataType(DataType.Text)]
         [StringLength(30, MinimumLength = 5)]
         [Display(Name = "Username")]
         public string Username { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Este campo es requerido")]
         [DataType(DataType.Password)]
         [MembershipPassword]
-        [Display(Name = "Password")]
+        [Display(Name = "Contraseña")]
         public string Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Este campo es requerido")]
         [Compare("Password")]
         [DataType(DataType.Password)]
         [MembershipPassword]
-        [Display(Name = "Confirm Password")]
+        [Display(Name = "Confirmar Contraseña")]
         public string ConfirmPassword { get; set; }
 
         [Display(Name = "Would you like to receive news about Topodata?")]
