d3b077455dc30564f08771ed8e07a86ef15c4977
kelfibm <kelfi.bm@gmail.com>
2016-05-21 19:46:44 -0400

    ServicesController added MustLogIn view added

---
+++ Topodata2/Controllers/ServicesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Topodata2.Controllers
+{
+    public class ServicesController : Controller
+    {
+        // GET: Services
+        public ActionResult Index()
+        {
+            if (Request.IsAuthenticated)
+            {
+                
+            }
+            return View();
+        }
+    }
+}
