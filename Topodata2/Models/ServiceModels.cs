fb4fd7acac58312035c012125437eb24af7400cb
kelfibm <kelfi.bm@gmail.com>
2016-05-23 08:55:29 -0400

    ServiceModel, document view added UserViewModel moved

---
+++ Topodata2/Models/ServiceModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Topodata2.Models
+{
+    public class ServiceModels
+    {
+
+    }
+
+    public class ServiceDocumentViewModel
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
