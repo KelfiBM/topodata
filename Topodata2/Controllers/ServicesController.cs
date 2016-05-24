ecdf2f4c86041a626c3374d828d0ac847344c22b
kelfibm <kelfi.bm@gmail.com>
2016-05-23 20:05:17 -0400

    GetDocumentById added ServiceDocument model added

--- Topodata2/Controllers/ServicesController.cs
+++ Topodata2/Controllers/ServicesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Topodata2.Models;
 
 namespace Topodata2.Controllers
 {
@@ -18,6 +19,19 @@ namespace Topodata2.Controllers
             else
             {
                 return View("MustLogIn");
+            }
+        }
+
+        public ActionResult Document(int id)
+        {
+            var serviceDocument = new ServiceDocumentViewModel().GetDocumentById(id);
+            if (serviceDocument.Exists)
+            {
+                return View(serviceDocument);
+            }
+            else
+            {
+                return HttpNotFound();
             }
         }
     }
--- Topodata2/Models/ServiceModels.cs
+++ Topodata2/Models/ServiceModels.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Topodata2.Models
 {
@@ -13,7 +13,69 @@ namespace Topodata2.Models
     public class ServiceDocumentViewModel
     {
         public int Id { get; set; }
+        public string Nombre { get; set; }
         public string Descripcion { get; set; }
+        public string Categoria { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public string Url { get; set; }
+        public bool Exists { get; set; }
 
+        public ServiceDocumentViewModel GetDocumentById(int id)
+        {
+
+            string connection = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            ServiceDocumentViewModel serviceDocument = new ServiceDocumentViewModel();
+            try
+            {
+                SqlConnection con = new SqlConnection(connection);
+                SqlCommand com = new SqlCommand();
+                SqlDataReader reader;
+                com.CommandText =
+                    string.Format(
+                        "SELECT dbo.Documento.Nombre, " +
+                        "dbo.DetalleDocumento.Descripcion, " +
+                        "dbo.Categoria.Descripcion AS Categoria, " +
+                        "dbo.DetalleDocumento.FechaPublicacion, " +
+                        "dbo.DetalleDocumento.Url " +
+                        "FROM dbo.Categoria INNER JOIN dbo.DetalleDocumento " +
+                        "ON dbo.Categoria.IdCategoria = dbo.DetalleDocumento.IdCategoria " +
+                        "INNER JOIN dbo.Documento " +
+                        "ON dbo.DetalleDocumento.idDocumento = dbo.Documento.IdDocumento " +
+                        "WHERE(dbo.Documento.IdDocumento = '{0}')",
+                        id);
+                com.CommandType = CommandType.Text;
+                com.Connection = con;
+                con.Open();
+                reader = com.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    if (reader.Read())
+                    {
+                        serviceDocument.Nombre = reader.GetString(0);
+                        serviceDocument.Descripcion = reader.GetString(1);
+                        serviceDocument.Categoria = reader.GetString(2);
+                        serviceDocument.FechaPublicacion = reader.GetDateTime(3);
+                        serviceDocument.Url = reader.GetString(4);
+                        serviceDocument.Exists = true;
+                        return serviceDocument;
+                    }
+                    else
+                    {
+                        serviceDocument.Exists = false;
+                        return serviceDocument;
+                    }
+                }
+                else
+                {
+                    serviceDocument.Exists = false;
+                    return serviceDocument;
+                }
+            }
+            catch (Exception)
+            {
+                serviceDocument.Exists = false;
+                return serviceDocument;
+            }
+        }
     }
 }
